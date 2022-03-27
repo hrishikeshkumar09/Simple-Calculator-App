5e0a95aca9b1d6842b1d00809ebca56e7e1673e1
hunterace09 <hrishikeshkumar09@gmail.com>
2022-03-27 22:00:47 +0530

    React based simple calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [result,setResult]=useState("");
+
+  const clickHandler=(e)=>{
+    setResult(result+e.target.value);
+  }
+
+  const delHandler=()=>{
+    setResult(result.slice(0,result.length-1));
+  }
+
+  const clearHandler=()=>{
+    setResult("");
+  }
+
+  const resultHandler=()=>{
+    let i=0;
+    while(i<result.length)
+    {    
+        if(result.charAt(i)==='-' || result.charAt(i)==='+' || result.charAt(i)==='*' || result.charAt(i)==='/')
+          break;
+        i++;
+    }      
+    var k=parseFloat(result.slice(0,i));
+    var m=result.charAt(i);
+    var n=parseFloat(result.slice(i+1,result.length));
+    var str=k+m+n;
+    setResult(eval(str).toString());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="head">
+      <h1>CALCULATOR</h1>
     </div>
+    <div class='calc'>
+      <input type='text' placeholder='0' value={result} class='disp'/>
+      <div class='btnclass'>
+        <input type='button' value='/' class='btn' onClick={clickHandler}/>
+        <input type='button' value='*' class='btn' onClick={clickHandler}/>
+        <input type='button' value='-' class='btn' onClick={clickHandler}/>
+        <input type='button' value='+' class='btn' onClick={clickHandler}/>
+        <input type='button' value='.' class='btn' onClick={clickHandler}/>
+        <input type='button' value='9' class='btn' onClick={clickHandler}/>
+        <input type='button' value='8' class='btn' onClick={clickHandler}/>
+        <input type='button' value='7' class='btn' onClick={clickHandler}/>
+        <input type='button' value='6' class='btn' onClick={clickHandler}/>
+        <input type='button' value='5' class='btn' onClick={clickHandler}/>
+        <input type='button' value='4' class='btn' onClick={clickHandler}/>
+        <input type='button' value='3' class='btn' onClick={clickHandler}/>
+        <input type='button' value='2' class='btn' onClick={clickHandler}/>
+        <input type='button' value='1' class='btn' onClick={clickHandler}/>
+        <input type='button' value='0' class='btn' onClick={clickHandler}/>
+        <input type='button' value='Clear' class='btn1' onClick={clearHandler}/>
+        <input type='button' value='Del' class='btn1' onClick={delHandler}/>
+        <input type='button' value='=' class='btn1' onClick={resultHandler}/>
+      </div>
+    </div>
+    </>
   );
 }
 
